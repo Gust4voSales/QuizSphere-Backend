521425aa84c5238964c5c0bde43b0efa6ea54c33
Lucca Radaeli <radaelidev@gmail.com>
2020-06-26 22:58:30 -0300

    Redividindo recursos e adicionando botao insomnia

--- src/controllers/ActivitiesController.js
+++ src/controllers/ActivitiesController.js
@@ -1,48 +1,48 @@
-const FriendRelation = require('../models/FriendRelation');
-const ActivitiesNotifications = require('../models/ActivitiesNotifications');
-const deleteOldActivities = require('./utils/deleteOldActivities');
+const ActivitiesNotifications = require("../models/ActivitiesNotifications");
+const deleteOldActivities = require("./utils/deleteOldActivities");
 
 module.exports = {
-    async index(req, res) {
-        try {
-            const userId = req.userId;
-            const { page=1 } = req.query;
+  async index(req, res) {
+    try {
+      const userId = req.userId;
+      const { page = 1 } = req.query;
 
-            await deleteOldActivities();
+      await deleteOldActivities();
 
-            const activities = await ActivitiesNotifications.paginate(
-                { recipientUser: userId, },
-                {
-                    page,
-                    limit: 15,
-                    sort: { seen: 1, createdAt: -1 }
-                }
-            );
-            
-            return res.json({ activities, });
-        } catch (err) {
-            console.log(err);
-            
-            return res.status(400).json({ error: "Não foi possível buscar atividades." });
+      const activities = await ActivitiesNotifications.paginate(
+        { recipientUser: userId },
+        {
+          page,
+          limit: 15,
+          sort: { seen: 1, createdAt: -1 },
         }
-    },
-  
-    async update(req, res) {
-        try{
-            const activitiesIdsToUpdate = req.body;
-            
-            await ActivitiesNotifications.updateMany(
-                { _id: { $in: activitiesIdsToUpdate } },
-                { $set: { seen: true } },
-            );
-            // console.log(a);
-            return res.json({ message: "Activities updated" });
+      );
 
-        } catch(err) {
-            console.log('err', err);
-            return res.status(400).json({ error: "Not possible to update acitivies" });
-        }
-    },
+      return res.json({ activities });
+    } catch (err) {
+      console.log(err);
 
+      return res
+        .status(400)
+        .json({ error: "Não foi possível buscar atividades." });
+    }
+  },
 
-}
+  async update(req, res) {
+    try {
+      const { activitiesIdsToUpdate } = req.body;
+
+      await ActivitiesNotifications.updateMany(
+        { _id: { $in: activitiesIdsToUpdate } },
+        { $set: { seen: true } }
+      );
+
+      return res.json({ message: "Activities updated" });
+    } catch (err) {
+      console.log("err", err);
+      return res
+        .status(400)
+        .json({ error: "Not possible to update activities" });
+    }
+  },
+};
--- src/routes.js
+++ src/routes.js
@@ -1,54 +1,86 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const authMiddleware = require('./middlewares/auth');
-const registerInputValidationMiddleware = require('./middlewares/registerInputValidation');
-const createQuizValidationMiddleware = require('./middlewares/createQuizValidation');
+const authMiddleware = require("./middlewares/auth");
+const registerInputValidationMiddleware = require("./middlewares/registerInputValidation");
+const createQuizValidationMiddleware = require("./middlewares/createQuizValidation");
 
-const AuthController = require('./controllers/AuthController');
-const QuizController = require('./controllers/QuizController');
-const LikeQuizController = require('./controllers/LikeQuizController');
-const UserController = require('./controllers/UserController');
-const UserShareQuiz = require('./controllers/UserShareQuiz');
-const UserFavoriteQuizzes = require('./controllers/UserFavoriteQuizzesController');
-const ActivitiesController = require('./controllers/ActivitiesController');
-const FriendRelationController = require('./controllers/FriendRelationsControllers/FriendRelationController');
-const FriendInvitationsController = require('./controllers/FriendRelationsControllers/FriendInvitationsController');
-const AcceptFriendInvitationController = require('./controllers/FriendRelationsControllers/AcceptFriendInvitationController');
-const DeclineFriendInvitationController = require('./controllers/FriendRelationsControllers/DeclineFriendInvitationController');
+const AuthController = require("./controllers/AuthController");
+const QuizController = require("./controllers/QuizController");
+const LikeQuizController = require("./controllers/LikeQuizController");
+const UserController = require("./controllers/UserController");
+const UserShareQuiz = require("./controllers/UserShareQuiz");
+const UserFavoriteQuizzes = require("./controllers/UserFavoriteQuizzesController");
+const ActivitiesController = require("./controllers/ActivitiesController");
+const FriendRelationController = require("./controllers/FriendRelationsControllers/FriendRelationController");
+const FriendInvitationsController = require("./controllers/FriendRelationsControllers/FriendInvitationsController");
+const AcceptFriendInvitationController = require("./controllers/FriendRelationsControllers/AcceptFriendInvitationController");
+const DeclineFriendInvitationController = require("./controllers/FriendRelationsControllers/DeclineFriendInvitationController");
 
+// create a user
+router.post(
+  "/users/register",
+  registerInputValidationMiddleware,
+  AuthController.register
+);
 
-router.post('/auth/register', registerInputValidationMiddleware, AuthController.register);
-router.post('/auth/authenticate', AuthController.authenticate);
+// check this routes. Should not use auth?
+router.get("/users", UserController.index);
+router.get("/quiz/list", QuizController.index);
 
-router.get('/quiz', authMiddleware, QuizController.index);
-router.post('/quiz', authMiddleware, createQuizValidationMiddleware, QuizController.store);
-router.get('/quiz/:id', authMiddleware, QuizController.show);
+router.post("/auth/login", AuthController.authenticate);
 
-router.post('/quiz/:quizId/like', authMiddleware, LikeQuizController.store);
-router.delete('/quiz/:quizId/deslike', authMiddleware, LikeQuizController.destroy);
+// routes that need auth
+router.use(authMiddleware);
 
-router.get('/user/savedQuizzes', authMiddleware, UserFavoriteQuizzes.index);
-router.post('/user/savedQuizzes/:quizId', authMiddleware, UserFavoriteQuizzes.store);
-router.delete('/user/savedQuizzes/:quizId', authMiddleware, UserFavoriteQuizzes.destroy);
+// users
+router.get("/users/:id", UserController.show);
 
-router.get('/shareQuiz', authMiddleware, UserShareQuiz.index);
-router.post('/shareQuiz/:quizId', authMiddleware, UserShareQuiz.store);
+// quiz - create
+router.post(
+  "/quiz/create",
+  createQuizValidationMiddleware,
+  QuizController.store
+);
 
-router.get('/user/notifications', authMiddleware, ActivitiesController.index);
-router.put('/user/notifications/setSeenActivities', authMiddleware, ActivitiesController.update);
+// quiz - show
+router.get("/quiz/show/:id", QuizController.show);
 
-router.get('/user/friend', authMiddleware, FriendRelationController.index);
-router.post('/user/friend', authMiddleware, FriendRelationController.store);
-router.delete('/user/friend/:relationId', authMiddleware, FriendRelationController.destroy);
+// quiz - saves
+router.post("/quiz/saved/add/:quizId", UserFavoriteQuizzes.store);
+router.delete("/quiz/saved/remove/:quizId", UserFavoriteQuizzes.destroy);
+router.get("/quiz/saved", UserFavoriteQuizzes.index);
 
-router.get('/user/friend/pendingInvitations', authMiddleware, FriendInvitationsController.index);
+// quiz - likes
+router.post("/quiz/:quizId/like", LikeQuizController.store);
+router.delete("/quiz/:quizId/dislike", LikeQuizController.destroy);
 
-router.post('/user/friend/acceptInvitation/:recipientId', authMiddleware, AcceptFriendInvitationController.store);
-router.post('/user/friend/declineInvitation/:recipientId', authMiddleware, DeclineFriendInvitationController.store);
+// quiz - share
+router.post("/quiz/share/:quizId", UserShareQuiz.store);
+router.get("/quiz/received", UserShareQuiz.index);
 
-router.get('/user', UserController.index);
-router.get('/user/:id', authMiddleware, UserController.show);
+// friends - list all
+router.get("/friends", FriendRelationController.index);
 
+//friends - list requests
+router.get("/friends/pendingInvitations", FriendInvitationsController.index);
+
+// friends - add/remove
+router.post("/friends/add", authMiddleware, FriendRelationController.store);
+router.delete("/friends/remove/:relationId", FriendRelationController.destroy);
+
+// friends - accept / decline
+router.post(
+  "/friends/accept/:recipientId",
+  AcceptFriendInvitationController.store
+);
+router.post(
+  "/friends/decline/:recipientId",
+  DeclineFriendInvitationController.store
+);
+
+// notifications
+router.get("/notifications", ActivitiesController.index);
+router.put("/notifications/setasread", ActivitiesController.update);
 
 module.exports = router;
